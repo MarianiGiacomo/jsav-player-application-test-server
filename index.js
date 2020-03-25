ff7bccda7c68a70ac6136c09f30833acb48e926b
Mariani Giacomo <mariang1@t31300-lr138.org.aalto.fi>
2020-03-26 00:08:29 +0200

    ..

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const redirect = require("express-redirect");
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient;
 const ObjectID = mongodb.ObjectID;
+const aesjs = require('aes-js');
 const CryptoJS = require("crypto-js");
 
 /**
@@ -47,10 +48,15 @@ app.redirect("/exercises/:exercise(.*)", "/", "post")
 app.get("/", (req, res) => {
   const cipher = req.query['submission'];
   if(cipher) {
-    const bytes = CryptoJS.AES.decrypt(cipher, cryptoKey);
-    console.log('bytes: ', bytes);
-    const idHex = Buffer.from(bytes.words, 'utf8');
+    // const bytes = CryptoJS.AES.decrypt(cipher, cryptoKey);
+    // console.log('bytes: ', bytes);
+    // const idHex = Buffer.from(bytes.words, 'utf8');
     // const idHex = bytes.toString(CryptoJS.enc.Utf8);
+    console.log('received cipher', cipher);
+    const encryptedBytes = aesjs.utils.hex.toBytes(cipher);
+    const aesCtr = new aesjs.ModeOfOperation.ctr(key);
+    const decryptedBytes = aesCtr.decrypt(encryptedBytes);
+    const idHex =  = aesjs.utils.utf8.fromBytes(decryptedBytes);
     console.log('idHex: ', idHex);
     const _id = new ObjectID.createFromHexString(idHex);
     console.log('_id: ', _id)
@@ -77,7 +83,12 @@ app.post("/", (req, res) => {
   .then( resData => {
     let id = resData.insertedId.toHexString();
     console.log('id: ', id);
-    let cipher = CryptoJS.AES.encrypt(id, cryptoKey).toString();
+    // let cipher = CryptoJS.AES.encrypt(id, cryptoKey).toString();
+    const textBytes = aesjs.utils.utf8.toBytes(text);
+    const aesCtr = new aesjs.ModeOfOperation.ctr(key);
+    const encryptedBytes = aesCtr.encrypt(textBytes);
+    let cipher = = aesjs.utils.hex.fromBytes(encryptedBytes);
+    console.log('sent cipher', cipher)
     let urlParam = `${server}/?submission=${cipher}`;
     const iframe =
     `<iframe
