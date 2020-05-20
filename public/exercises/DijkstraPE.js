256dcc9c00a7d0d4c210d9d1081f959c20ab9e5e
Mariani Giacomo <mariang1@t31300-lr138.org.aalto.fi>
2020-05-20 16:22:15 +0300

    updating exercise

--- public/exercises/DijkstraPE.js
+++ public/exercises/DijkstraPE.js
@@ -398,7 +398,4 @@
   $(".jsavcontainer").on("click", ".jsavnode", function () {
     window.alert("Please, click on the edges, not the nodes.");
   });
-
-  $("#about").click(about);
-
 }(jQuery));
